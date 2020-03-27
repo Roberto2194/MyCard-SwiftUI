1e64c14defa30f09489bed26c9120d069f168fa2
Roberto Liccardo <roberto_liccardo@outlook.it>
2020-03-27 22:26:52 +0100

    Initial Commit

---
+++ RobertoCard - SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RobertoCard - SwiftUI
+//
+//  Created by Roberto Liccardo on 27/03/2020.
+//  Copyright © 2020 Roberto Liccardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
